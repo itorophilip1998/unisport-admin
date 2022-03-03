3facc7b3204b28ea4c9726bfc70d062ac5b89cf6
itphilip1998 <itkeyzzpianny@gmail.com>
2022-03-03 16:11:07 +0100

    dev

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import index from './views/register';
+// import { Header } from './layouts/Header';
+// import Footer from './layouts/Footer';
+// import Register from './views/register';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<index />} exact />
+//         <Route path="/register" component={<Register />} />
+
+//         {/* <Route path="/contact" element={<ContactForm />} /> */}
+//         {/* <Route path="/blogs" element={<Blogs/>} /> */}
+//         {/* <Route path="/blogs/:blogId" element={<Blog />} /> */}
+//         {/* <Route path="/blogs/post" element={<Post />} /> */}
+//         {/* <Route path="/getstarted" element={<ChoiceBox /> */}
+//         {/* <Route path="/join-us" element={<JoinUs/>} /> */}
+//         {/* <Route path="/employ-dev" element={<EmployDev/>} /> */}
+//         {/* <Route path="/find-dev" element={<FindDev/>} /> */}
+//         {/* <Route path="*" element={<NotFoundPage />} /> */}
+//       </Routes>
+
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Footer from "./layouts/Footer";
+import { Header } from "./layouts/Header";
+import Home from "./views/home";
+import Register from "./views/Register";
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+
+    <Routes>
+      <Route path="/register" element={<Register />} />
+    </Routes>
+
+    <Footer />
+  </BrowserRouter>,
+  rootElement
+);
